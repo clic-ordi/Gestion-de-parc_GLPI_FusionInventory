ad9541988a3bb2a8af822ee7afcbe5725265527f
Maxime M <maxime.moineau@free.fr>
2017-08-10 15:53:39 +0200

    -Langage+en

--- FusionInventory_Agent_for_OSX/install.sh
+++ FusionInventory_Agent_for_OSX/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ######################FusionInventory-Agent setup script####
-LANGAGE="fr"
-LINK="https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.21/FusionInventory-Agent-2.3.21-1.pkg.tar.gz"
+LINK='https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.3.21/FusionInventory-Agent-2.3.21-1.pkg.tar.gz'
 ############################################################
 
 # FusionInventory-Agent setup script for osx using download link :
@@ -10,7 +9,7 @@ PWD=$(pwd);
 echo'############Installing homebrew#############################'
 #Checking homebrew is present 
 HOMEBREW=$(brew -v)
-echo "DEBUG : $PWD $HOMEBREW +++++++++++++++++++++++++++";
+echo "DEBUG : $PWD ; $HOMEBREW "
 if [ $(HOMEBREW) != '']; then
 	echo "# homebrew is already installed"
 else
@@ -23,9 +22,10 @@ echo '############Downloading the agent installer########################';
 brew install wget;
 mkdir /opt/fusioninventory-agent_temp;
 cd /opt/fusioninventory-agent_temp;
+echo "DEBUG : $LINK "
 wget $(LINK);
 tar xfz fusioninventory-agent_macosx-intel_*.pkg.tar.gz;
-sudo installer -pkg FusionInventory-Agent.pkg -target / -lang $(LANGAGE);
+sudo installer -pkg FusionInventory-Agent.pkg -target / -lang en;
 echo '############Terminating#####################################';
 rm -rf /opt/fusioninventory-agent_temp;
 cd $(PWD);
